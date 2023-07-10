005908615389364a17c056c1b9757d58736c73d7
dgj <dgj@qq.com>
2023-07-10 17:43:20 +0800

    springCloud-0.2

--- eureka-01/eureka-client-a/src/main/java/com/duguanjun/EurekaClientAApplication.java
+++ eureka-01/eureka-client-a/src/main/java/com/duguanjun/EurekaClientAApplication.java
@@ -1,15 +1,23 @@
 package com.duguanjun;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class EurekaClientAApplication {
+public class EurekaClientAApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClientAApplication.class, args);
 	}
 
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+
+	}
 }
