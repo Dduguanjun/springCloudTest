4b3f8477b32777a65a90eec0bfcbade65e3a26e8
dgj <dgj@qq.com>
2023-07-10 17:44:34 +0800

    springCloud-0.3

--- eureka-01/eureka-client-a/src/main/java/com/duguanjun/EurekaClientAApplication.java
+++ eureka-01/eureka-client-a/src/main/java/com/duguanjun/EurekaClientAApplication.java
@@ -19,5 +19,7 @@ public class EurekaClientAApplication implements ApplicationRunner {
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 
+		System.out.println("");
+
 	}
 }
